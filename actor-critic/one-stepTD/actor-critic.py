6efe0223bbf05625ec91e6c03d863bb807246cdc
Ken Ming Lee <leekenming99@gmail.com>
2020-09-07 19:22:44 -0400

    one-step TD vanilla actor-critic solved cartpole:)

--- actor-critic/one-stepTD/actor-critic.py
+++ actor-critic/one-stepTD/actor-critic.py
@@ -3,6 +3,7 @@ from run_experiment import run_cartpole_experiment
 import torch.nn as nn
 import torch
 import torch.optim as optim
+from torch.distributions import Categorical
 import numpy as np
 import random
 import torch.nn.functional as F
@@ -12,27 +13,21 @@ import gym
 
 
 class Critic(nn.Module):
-    # Dueling architecture https://arxiv.org/pdf/1511.06581.pdf
-    def __init__(self, input_size, n_actions):
-        super(DQN, self).__init__()
-        self.n_actions = n_actions
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc_state_action_val_output = nn.Linear(128, n_actions)
-        self.fc_state_val_output = nn.Linear(128, 1)
+    def __init__(self, input_size):
+        super(Critic, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        stream1_out = self.fc_state_action_val_output(x)
-        stream2_out = self.fc_state_val_output(x)
-        stream1_out_mean = torch.sum(
-            stream1_out, dim=1, keepdim=True) / self.n_actions
-        output = stream2_out + (stream1_out - stream1_out_mean)
-        return output
+        return self.net(x)
 
 
 class Actor(nn.Module):
     def __init__(self, input_size, n_actions):
-        super(DQN, self).__init__()
+        super(Actor, self).__init__()
         self.net = nn.Sequential(
             nn.Linear(input_size, 128),
             nn.ReLU(),
@@ -43,18 +38,56 @@ class Actor(nn.Module):
         return self.net(x)
 
 
-class agent(base_agent):
+class ActorCriticAgent(base_agent):
     def __init__(self, input_size, actions, learning_rate=0.001, reward_decay=0.9):
         self.device = torch.device(
             'cuda' if torch.cuda.is_available() else 'cpu')
         self.actor = Actor(input_size, len(actions)
                            ).double().to(device=self.device)
-        self.critic = Critic(input_size, len(
-            actions)).double().to(device=self.device)
-        
+        self.critic = Critic(input_size).double().to(device=self.device)
+        self.gamma = reward_decay
+        self.actor_optimizer = optim.Adam(
+            self.actor.parameters(), lr=learning_rate)
+        self.critic_optimizer = optim.Adam(
+            self.critic.parameters(), lr=learning_rate)
 
     def choose_action(self, s) -> int:
-        pass
+        s = torch.tensor(s, dtype=torch.double).to(device=self.device)
+        logits = self.actor(s)
+        probs = Categorical(logits=logits)
+        a = probs.sample()
+        return a.tolist()
 
     def learn(self, s, a, r, s_, done) -> int:
-        pass
+        a_ = self.choose_action(s_)
+        s = torch.tensor(s, dtype=torch.double).to(device=self.device)
+        s_ = torch.tensor(s_, dtype=torch.double).to(device=self.device)
+        a = torch.tensor(a, device=self.device)
+        if done:
+            delta = r - self.critic(s)
+        else:
+            delta = r + self.gamma * self.critic(s_) - self.critic(s)
+
+        self.actor_optimizer.zero_grad()
+        m = Categorical(logits=self.actor(s))
+        actor_loss = -m.log_prob(a)*(delta.detach())
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        self.critic_optimizer.zero_grad()
+        critic_loss = delta**2
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        return a_
+
+    def save_model(self, path):
+        torch.save(self.actor, path)
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    agent = ActorCriticAgent(env.observation_space.shape[0], [
+        i for i in range(env.action_space.n)])
+    run_cartpole_experiment(agent)
+    agent.save_model('vanilla_actor_critic.pt')
