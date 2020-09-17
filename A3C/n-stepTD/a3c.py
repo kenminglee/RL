5a53e49b629adf59cbab65e3ee2094a3c292b5dc
Ken Ming Lee <leekenming99@gmail.com>
2020-09-17 19:09:34 -0400

    fixed a3c nstep td

--- A3C/n-stepTD/a3c.py
+++ A3C/n-stepTD/a3c.py
@@ -52,21 +52,22 @@ class Agent(base_agent):
 
     def learn(self, s, a, r, s_, done) -> int:
         self.obs.append((s, self.latest_log_prob.pop(), r,))
-        if done:
+        if done or len(self.obs) >= self.n_step:
             self.perform_learning_iter(s_, done)
             self.obs = []
-            return 0
-        return self.choose_action(s_)
+        return 0 if done else self.choose_action(s_)
 
     def perform_learning_iter(self, s_, done):
         states, log_probs, rewards = zip(*self.obs)
         states = torch.tensor(states, dtype=torch.double).to(
             device=self.device)
         if not done:
-            rewards = list(rewards)
-            _, val = self.agent(s_)
-            rewards[-1] += self.gamma*val
-        discounted_rewards = self.convert_to_discounted_reward(rewards)
+            _, val = self.agent(torch.tensor(
+                s_, dtype=torch.double).to(device=self.device))
+            discounted_rewards = self.convert_to_discounted_reward(
+                rewards, val)
+        else:
+            discounted_rewards = self.convert_to_discounted_reward(rewards, 0)
         discounted_rewards = torch.tensor(
             discounted_rewards, device=self.device)
         log_probs = torch.stack(
@@ -86,8 +87,8 @@ class Agent(base_agent):
 
         self.agent.load_state_dict(self.gl_agent.state_dict())
 
-    def convert_to_discounted_reward(self, rewards):
-        cumulative_reward = [rewards[-1]]
+    def convert_to_discounted_reward(self, rewards, next_state_val):
+        cumulative_reward = [rewards[-1]+self.gamma*next_state_val]
         for reward in reversed(rewards[:-1]):
             cumulative_reward.append(reward + self.gamma*cumulative_reward[-1])
         return cumulative_reward[::-1]
@@ -139,7 +140,7 @@ if __name__ == "__main__":
     gl_print_lock = Lock()
     for i in range(mp.cpu_count()):
         p = mp.Process(target=worker, args=(
-            gl_agent, gl_r_per_eps, gl_solved, gl_lock, gl_print_lock, i, 1))
+            gl_agent, gl_r_per_eps, gl_solved, gl_lock, gl_print_lock, i, 10))
         p.start()
         processes.append(p)
     for p in processes:
