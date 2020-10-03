48d75971adf4b8d2c73f56e55e86fba7786b6bf1
ken <leekenming99@gmail.com>
2020-10-03 19:04:02 -0400

    minor changes

--- A2C/cartpole/a2c.py
+++ A2C/cartpole/a2c.py
@@ -10,70 +10,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # A3C paper: https://arxiv.org/pdf/1602.01783.pdf
-
+# A2C blogpost: https://openai.com/blog/baselines-acktr-a2c/
 
 class ActorCritic(nn.Module):
     def __init__(self, input_size, n_actions):
         super(ActorCritic, self).__init__()
         self.first_layer = nn.Linear(input_size, 128)
-        self.critic_head = nn.Linear(128, 1)
+        self.critic_layer = nn.Linear(128, 64)
+        self.critic_head = nn.Linear(64, 1)
         self.actor_head = nn.Linear(128, n_actions)
 
     def forward(self, x):
         x = F.relu(self.first_layer(x))
-        return self.actor_head(x), self.critic_head(x.detach())
+        return self.actor_head(x), self.critic_head(F.relu(self.critic_layer(x.detach())))
 
 
 class Agent(base_agent):
-    def __init__(self, env, gl_agent, gl_lock, n_step=10, reward_decay=0.9):
-        # self.device = torch.device(
-        #     'cuda' if torch.cuda.is_available() else 'cpu')
+    def __init__(self, agent, n_step=10, reward_decay=0.9):
         self.device = torch.device('cpu')
         self.gamma = reward_decay
         self.agent = ActorCritic(
             env.observation_space.shape[0], env.action_space.n).double().to(device=self.device)
-        self.agent.load_state_dict(gl_agent.state_dict())
-        self.gl_optim = optim.Adam(gl_agent.parameters())
-        self.gl_agent = gl_agent
-        self.gl_lock = gl_lock
         self.obs = []
         self.n_step = n_step
         self.latest_log_prob = []
+        self.latest_entropy = []
 
     def choose_action(self, s) -> int:
         s = torch.tensor(s, dtype=torch.double).to(device=self.device)
         actor_logits, _ = self.agent(s)
         probs = Categorical(logits=actor_logits)
         a = probs.sample()
-        assert len(self.latest_log_prob) == 0
+        assert len(self.latest_log_prob) == 0 and len(self.latest_entropy)==0
         self.latest_log_prob.append(probs.log_prob(a))
+        self.latest_entropy.append(probs.entropy())
         return a.tolist()
 
     def learn(self, s, a, r, s_, done) -> int:
-        self.obs.append((s, self.latest_log_prob.pop(), r,))
-        if done:
+        self.obs.append((s, self.latest_log_prob.pop(), r, self.latest_entropy.pop()))
+        if done or len(self.obs) >= self.n_step:
             self.perform_learning_iter(s_, done)
             self.obs = []
-            return 0
-        return self.choose_action(s_)
+        return 0 if done else self.choose_action(s_)
 
     def perform_learning_iter(self, s_, done):
-        states, log_probs, rewards = zip(*self.obs)
+        states, log_probs, rewards, entropy = zip(*self.obs)
         states = torch.tensor(states, dtype=torch.double).to(
             device=self.device)
         if not done:
-            rewards = list(rewards)
-            _, val = self.agent(s_)
-            rewards[-1] += self.gamma*val
-        discounted_rewards = self.convert_to_discounted_reward(rewards)
+            _, val = self.agent(torch.tensor(
+                s_, dtype=torch.double).to(device=self.device))
+            discounted_rewards = self.convert_to_discounted_reward(
+                rewards, val)
+        else:
+            discounted_rewards = self.convert_to_discounted_reward(rewards, 0)
         discounted_rewards = torch.tensor(
             discounted_rewards, device=self.device)
         log_probs = torch.stack(
             log_probs).to(device=self.device)
+        entropy = torch.stack(entropy).to(device=self.device)
         _, critic_values = self.agent(states)
         delta = discounted_rewards - critic_values
-        loss = torch.sum(-log_probs*(delta.detach())) + \
-            0.1*torch.mean(delta**2)
+        loss = torch.mean(-log_probs*(delta.detach()) - 0.001*entropy + 0.1*(delta**2))
 
         self.gl_lock.acquire()
         self.gl_optim.zero_grad()
@@ -85,20 +83,22 @@ class Agent(base_agent):
 
         self.agent.load_state_dict(self.gl_agent.state_dict())
 
-    def convert_to_discounted_reward(self, rewards):
-        cumulative_reward = [rewards[-1]]
+    def convert_to_discounted_reward(self, rewards, next_state_val):
+        cumulative_reward = [rewards[-1]+self.gamma*next_state_val]
         for reward in reversed(rewards[:-1]):
             cumulative_reward.append(reward + self.gamma*cumulative_reward[-1])
         return cumulative_reward[::-1]
+    
+    def load_state_dict(self, state_dict):
+        self.agent.load_state_dict(state_dict)
 
-
-def run(gl_return, gl_agent, gl_agent_lock, gl_print_lock, gl_count, semaphore, worker_num, gl_solved, n_step):
+def run(gl_return, gl_agent, gl_agent_lock, gl_print_lock, gl_count, semaphore, worker_num, gl_solved):
     env = gym.make("CartPole-v0")
     # env = gym.wrappers.Monitor(env, "recording", force=True)
     device = torch.device('cpu')
-    agent = ActorCritic(
+    network = ActorCritic(
             env.observation_space.shape[0], env.action_space.n).double().to(device=self.device)
-    agent.load_state_dict(gl_agent.state_dict())
+    agent = Agent(network, gl_agent)
     r_per_eps = []
     
     while not bool(gl_solved.value):
@@ -147,7 +147,7 @@ def training_loop():
     processes = []
     reward = []
     for i in range(num_processes):
-        p = mp.Process(target=worker, args=(gl_ret, gl_agent, gl_agent_lock, gl_print_lock, gl_count, semaphore, i, gl_solved, 1))
+        p = mp.Process(target=run, args=(gl_ret, gl_agent, gl_agent_lock, gl_print_lock, gl_count, semaphore, i, gl_solved))
         p.start()
         processes.append(p)
     for p in processes:
